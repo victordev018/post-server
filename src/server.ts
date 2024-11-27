5b477dc20bca220cda4bd49ee78905f8fd427d16
João VIctor <joawrt8520@gmail.com>
2024-11-26 23:34:59 -0300

    feat: add local repository with static data and created two enpoints to find posts

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response } from "express";
+import {posts} from "./repository/data";
 
 const app = express();
 
@@ -10,6 +11,20 @@ app.listen(port, () => {
     console.log(`Server is running in: http://localhost:${port}`);
 });
 
-app.get("/api", (req: Request, res:Response) => {
-    res.status(200).json("hello world");
+app.get("/posts", (req: Request, res:Response) => {
+    res.status(200).json(posts);
+})
+
+function getPostById(id:number) {
+    const index = posts.findIndex((post) => {
+        return post.id === id;
+    });
+
+    return posts[index];
+}
+
+app.get("/posts/:id", (req: Request, res:Response) => {
+    const id : number = Number.parseInt(req.params.id);
+    const response = getPostById(id);
+    res.status(200).json(response);
 })
